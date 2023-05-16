7464e90a677a1e4242ecaae7a9de73b22f7cd641
afarovruslan <afarovruslan@gmail.com>
2023-05-16 13:08:20 +0300

    add gallery

--- scripts/index.js
+++ scripts/index.js
@@ -1,20 +1,20 @@
 
-function ChangeMode(class_name, first_mode = "grey-mode", second_mode = "less-black-mode") {
+function changeMode(class_name, first_mode = "grey-mode", second_mode = "less-black-mode") {
     let element_arr = document.querySelectorAll('.' + class_name);
     element_arr.forEach((element) => {
         element.classList.toggle(first_mode);
         element.classList.toggle(second_mode);
     });
 }
-function ChangeTheme() {
-    ChangeMode("page", "white-mode", "black-mode");
-    ChangeMode("menu__item", "white-mode", "black-mode");
-    ChangeMode("header");
-    ChangeMode("skills");
+function changeTheme() {
+    changeMode("page", "white-mode", "black-mode");
+    changeMode("menu__item", "white-mode", "black-mode");
+    changeMode("header");
+    changeMode("skills");
 }
 
 const change_theme_button = document.querySelector(".dark_theme_button");
-change_theme_button.addEventListener("click", ChangeTheme);
+change_theme_button.addEventListener("click", changeTheme);
 
 
 const rain_button = document.querySelector(".footer__button-beer");
