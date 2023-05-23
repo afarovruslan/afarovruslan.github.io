ca16823e838b4d0538e3d5a709161f7699069810
afarovruslan <afarovruslan@gmail.com>
2023-05-23 14:12:39 +0300

    fix dark theme

--- scripts/change_theme.js
+++ scripts/change_theme.js
@@ -5,19 +5,19 @@ function changeMode(class_name, first_mode = "grey-mode", second_mode = "less-bl
         element.classList.toggle(second_mode);
     });
 }
+
 function changeTheme() {
     console.log(localStorage.getItem("is_black"));
     changeMode("page", "white-mode", "black-mode");
     changeMode("menu__item", "white-mode", "black-mode");
     changeMode("header");
     changeMode("skills");
-    if (!localStorage["is_black"]) {
-        console.log('set black')
-        localStorage["is_black"] = true;
-    }
-    else if (localStorage["is_black"]) {
-        localStorage["is_black"] = false;
-        console.log('set white');
+    if (localStorage.getItem("is_black") === "0") {
+        localStorage.setItem("is_black", "1");
+        console.log("set black");
+    } else {
+        localStorage.setItem("is_black", "0");
+        console.log("set white");
     }
     console.log(localStorage.getItem("is_black"));
 }
@@ -26,10 +26,12 @@ const change_theme_button = document.querySelector(".menu__button");
 change_theme_button.addEventListener("click", changeTheme);
 
 function setTheme() {
-    console.log(localStorage.getItem("is_black"));
-    if (localStorage.getItem("is_black")) {
-        console.log('notice black');
+    if (localStorage.getItem("is_black") === "1") {
+        console.log("изначально черный")
         changeTheme();
+        localStorage.setItem("is_black", "1");
+    } else {
+        console.log("изначально белый");
     }
 }
 
