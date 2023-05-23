5010f5202587b5ac0ef267f097e02575c633da8c
afarovruslan <afarovruslan@gmail.com>
2023-05-23 13:50:28 +0300

    add dark theme after  updating

--- scripts/change_theme.js
+++ scripts/change_theme.js
@@ -6,11 +6,31 @@ function changeMode(class_name, first_mode = "grey-mode", second_mode = "less-bl
     });
 }
 function changeTheme() {
+    console.log(localStorage.getItem("is_black"));
     changeMode("page", "white-mode", "black-mode");
     changeMode("menu__item", "white-mode", "black-mode");
     changeMode("header");
     changeMode("skills");
+    if (!localStorage["is_black"]) {
+        console.log('set black')
+        localStorage["is_black"] = true;
+    }
+    else if (localStorage["is_black"]) {
+        localStorage["is_black"] = false;
+        console.log('set white');
+    }
+    console.log(localStorage.getItem("is_black"));
 }
 
 const change_theme_button = document.querySelector(".menu__button");
 change_theme_button.addEventListener("click", changeTheme);
+
+function setTheme() {
+    console.log(localStorage.getItem("is_black"));
+    if (localStorage.getItem("is_black")) {
+        console.log('notice black');
+        changeTheme();
+    }
+}
+
+setTheme();
